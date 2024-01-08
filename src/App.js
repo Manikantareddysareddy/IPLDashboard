559fe2e24a59450427f42b2f1d9d9d7671a93f64
Manikantareddysareddy <149862326+Manikantareddysareddy@users.noreply.github.com>
2024-01-08 11:40:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Home from './components/Home'
 
 import TeamMatches from './components/TeamMatches'
 
+import NotFound from './components/NotFound'
+
 import './App.css'
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route exact component={NotFound} />
   </Switch>
 )
 
