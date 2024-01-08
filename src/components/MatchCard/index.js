d554712a9923022b1ef2f481282a57d32ef4ec13
Manikantareddysareddy <149862326+Manikantareddysareddy@users.noreply.github.com>
2024-01-08 11:45:19 +0530

    Update index.js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,35 @@
 // Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {matchItem} = props
+  const {
+    umpires,
+    result,
+    manOfTheMatch,
+    date,
+    venue,
+    competingTeam,
+    competingTeamLogo,
+    firstInnings,
+    secondInnings,
+    matchStatus,
+  } = matchItem
+
+  const matchStatusClassName = matchStatus === 'Won' ? 'won-para' : 'loss-para'
+
+  return (
+    <li className="recent-list-container">
+      <img
+        src={competingTeamLogo}
+        className="recent-image"
+        alt={`competing team ${competingTeam}`}
+      />
+      <p className="competing-heading">{competingTeam}</p>
+      <p className="competing-para">{result}</p>
+      <p className={` ${matchStatusClassName}`}>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
